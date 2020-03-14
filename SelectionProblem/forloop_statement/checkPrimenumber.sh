f683c7ae0822ccc6704aec5a3e501e2adca296b2
aashishogale <aashishogale9@gmail.com>
2020-03-14 16:23:45 +0530

    Added: Take a one number and check this prime or not

---
+++ SelectionProblem/forloop_statement/checkPrimenumber.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+read -p "Enter number for checking prime or not : " number
+count=0
+for (( i=1;i<=$number;i++ ))
+do
+	if [ $(($number%$i)) -eq 0 ];
+	then
+		(( count++ ))	
+
+fi
+done
+if [ $count -eq 2 ];
+then
+	echo "prime number"
+else
+	echo "not prime number"
+fi
+	
