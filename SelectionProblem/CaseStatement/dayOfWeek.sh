9472978642cb132e7186bfde298477dbfde3e515
aashishogale <aashishogale9@gmail.com>
2020-03-14 10:14:05 +0530

    Added: Read a number and display a week day

---
+++ SelectionProblem/CaseStatement/dayOfWeek.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+read -p "enter the number" number
+case $number in
+		0)
+			echo "Sunday"
+			;;
+		1)
+			echo "Monday"
+			;;
+		2)
+			echo "Tuesday"
+			;;
+		3)
+			echo "Wednesday"
+			;;
+		4)
+			echo "Thusday"
+			;;
+		5)
+			echo "Friday"
+			;;
+		6)
+			echo "Saturday"
+			;;
+		*)
+			echo "invalid"
+			;;
+esac
