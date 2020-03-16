d819f2e350e62e07275a2f37a36975c05900fd42
aashishogale <aashishogale9@gmail.com>
2020-03-16 16:24:09 +0530

    Added: Flip coin till either heads or tails wins 11 times

---
+++ SelectionProblem/whileloop_statement/flipCoin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x 
+Heads=0
+Tails=0
+count=0
+while [ true ]
+do
+	number=$((RANDOM%2))
+	if [ $number -eq 1 ]
+	then
+		(( Heads++ ))
+	else
+		(( Tails++ ))
+	fi
+if [ $Heads -eq 11 ]
+then
+	echo "Heads Win"
+	break
+elif [ $Tails -eq 11 ]
+then
+	echo "Tails win"
+	break
+fi
+(( count++ ))
+done
