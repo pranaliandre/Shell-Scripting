5f682757434b76113d967ab2e0f05dddf5cc5d45
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:04:07 +0530

    Refactor: Fixed Indentation

--- SequencesPractice1/dayofweek.sh
+++ SequencesPractice1/dayofweek.sh
@@ -9,9 +9,9 @@ x=$(( $y0 + $y0 / 4 - $y0 / 100 + $y0 / 400 ))
 m0=$(( $m + 12 * (( 14 - $m ) / 12) - 2 ))
 day0=$(( ( $d + $x + 31 * $m0 / 12 ) % 7 ))
 case $day0 in
-	0)
-		echo "Day of the date $@ is Sunday"
-		;;
+   0)
+      echo "Day of the date $@ is Sunday"
+      ;;
    1)
       echo "Day of the date $@ is Monday"
       ;;
@@ -26,11 +26,11 @@ case $day0 in
       ;;
    5)
       echo "Day of the date $@ is Friday"
-		;;
-	6)
+      ;;
+   6)
       echo "Day of the date $@ is Saturday"
       ;;
    *)
       echo "Invalid day"
-		;;
+      ;;
 esac
