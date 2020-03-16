f3b1346a7e34717ef962662c4ca1bdbd812954a8
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:19:22 +0530

    Refactor: Fixed Indentation

--- SelectionProblem/if_selection/leapYear.sh
+++ SelectionProblem/if_selection/leapYear.sh
@@ -2,10 +2,10 @@
 read -p "enter year" year
 if [ $year -ge 1000 ] && [ $year -le 9999 ]
 then
-		if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; 
-		then
-				echo "leap year"
-		else
-				echo "not a leap year"
-		fi
+	if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; 
+	then
+		echo "leap year"
+	else
+		echo "not a leap year"
+	fi
 fi
