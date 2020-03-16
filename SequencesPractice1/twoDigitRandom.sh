26a7e242cb96dba82187a60ee401b0bac78ad5f3
aashishogale <aashishogale9@gmail.com>
2020-03-16 14:58:22 +0530

    Refactor: fixed Indentation

--- SequencesPractice1/twoDigitRandom.sh
+++ SequencesPractice1/twoDigitRandom.sh
@@ -4,13 +4,13 @@ count=0;
 value=0;
 while [ $count != 5 ]
 do
-		value=$((RANDOM % 100))
-		if [ $((value/10)) -ge 1 ]
-		then
-				echo $value	
-				sum=$((sum+value))
-				((count++))
-		fi
+	value=$((RANDOM % 100))
+	if [ $((value/10)) -ge 1 ]
+	then
+		echo $value	
+		sum=$((sum+value))
+		((count++))
+	fi
 done		
 avg=$(echo "scale=2; $sum / $count" | bc)
 	
