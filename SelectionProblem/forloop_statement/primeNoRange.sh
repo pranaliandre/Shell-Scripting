0d65b5195e9dab0b34d98fac177999b8aa6a19b8
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:52:27 +0530

    Refactor: Print the prime factors of number N

---
+++ SelectionProblem/forloop_statement/primeNoRange.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+read -p "Enter the starting number: " begin
+read -p "Enter the ending number: " end
+for (( i=$begin;$i<=$end;i++))
+do
+	count=0
+	for (( j=1;$j<=$i;j++ ))
+	do
+		if [ $(($i%$j)) -eq 0 ]
+		then
+			(( count++ ))
+		fi
+	done
+	if [ $count -eq 2 ]
+	then
+		echo $i
+	fi
+done
+
