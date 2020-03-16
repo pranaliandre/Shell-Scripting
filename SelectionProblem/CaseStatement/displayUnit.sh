24e88759fea2ff3c7c8c09143c2bddf685f555d7
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:11:10 +0530

    Refactor: Fixed Indentation

--- SelectionProblem/CaseStatement/displayUnit.sh
+++ SelectionProblem/CaseStatement/displayUnit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -x
 read -p "enter the number 1,10,100,1000 : " no
 case $no in
-		1)
-			echo "unit"
-			;;
-		10)
-			echo "Ten"
-			;;
-		100)
-			echo "Hundred"
-			;;
-		1000)
-			echo "One Thousand"
-			;;
-		*)
-			echo "invalid range"
-			;;
+	1)
+		echo "unit"
+		;;
+	10)
+		echo "Ten"
+		;;
+	100)
+		echo "Hundred"
+		;;
+	1000)
+		echo "One Thousand"
+		;;
+	*)
+		echo "invalid range"
+		;;
 esac
