66a07c83eb010ee8f351f43874b8518b329cf53b
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:13:29 +0530

    Refactor: Fixed Indentation

--- SelectionProblem/CaseStatement/singleDigitWord.sh
+++ SelectionProblem/CaseStatement/singleDigitWord.sh
@@ -1,37 +1,37 @@
 #!/bin/bash -x
 singleDigit=$((RANDOM%10))
 case $singleDigit in
-		0)
-			echo "Zero"
-			;;
-		1)
-			echo "One"
-			;;
-		2)
-			echo "Two"
-			;;
-		3)
-			echo "Three"
-			;;
-		4)
-			echo "Four"
-			;;
-		5)
-			echo "Five"
-			;;	
-		6)
-			echo "Six"
-			;;
-		7)
-			echo "Seven"
-			;;
-		8)
-			echo "Eigth"
-			;;			
-		9)
-			echo "Nine"
-			;;
-		*)
-			echo "invalid"
-			;;
-		esac
+	0)
+		echo "Zero"
+		;;
+	1)
+		echo "One"
+		;;
+	2)
+		echo "Two"
+		;;
+	3)
+		echo "Three"
+		;;
+	4)
+		echo "Four"
+		;;
+	5)
+		echo "Five"
+		;;	
+	6)
+		echo "Six"
+		;;
+	7)
+		echo "Seven"
+		;;
+	8)
+		echo "Eigth"
+		;;			
+	9)
+		echo "Nine"
+		;;
+	*)
+		echo "invalid"
+		;;
+esac
