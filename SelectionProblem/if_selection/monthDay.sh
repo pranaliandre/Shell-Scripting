5f25cef353b82d700a923c5c00a88c1cdf1dfd95
aashishogale <aashishogale9@gmail.com>
2020-03-16 15:29:35 +0530

    Refactor: Fixed Indentation and changes in if condition

--- SelectionProblem/if_selection/monthDay.sh
+++ SelectionProblem/if_selection/monthDay.sh
@@ -3,40 +3,40 @@ read -p "enter day : " day
 read -p "enter month : " month
 if [ $month -eq 3 ];
 then
-		if [ $day -ge 20 ] || [ $day -le 31 ] 
-		then
-				echo true
-		else
-				echo false
-		fi
+	if [ $day -ge 20 ] && [ $day -le 31 ] 
+	then
+		echo true
+	else
+		echo false
+	fi
 fi
 if [ $month -eq 4 ];
 then
-      if [ $day -ge 1 ] || [ $day -le 30 ] 
-      then
-            echo true
-      else
-            echo false
-      fi
+	if [ $day -ge 1 ] && [ $day -le 30 ] 
+	then
+		echo true
+	else
+		echo false
+	fi
 fi
 
 if [ $month -eq 5 ];
 then 
-		if [ $day -ge 1 ] || [ $day -le 31 ] 
-      then
-            echo true
-      else
-            echo false
-      fi
+	if [ $day -ge 1 ] && [ $day -le 31 ] 
+	then
+		echo true
+	else
+		echo false
+	fi
 fi
 if [ $month -eq 6 ];
 then
- 		if [ $day -ge 1 ] || [ $day -le 20 ] 
-      then
-            echo true
-      else
-            echo false
-      fi
+ 	if [ $day -ge 1 ] && [ $day -le 20 ] 
+	then        
+		echo true
+	else
+		echo false
+	fi
 fi
 
 
