8b4f37c65fe4480d0013f485698ea7dee8eda554
pranaliandre <pranaliandre06@gmail.com>
2020-03-19 08:09:50 -0500

    Refactor: To assign arithmetic operation to a variable

--- SequencesPractice1/ArithmeticOperation.sh
+++ SequencesPractice1/ArithmeticOperation.sh
@@ -3,9 +3,9 @@ read -p "Enter a: " a
 read -p "Enter b: " b
 read -p "Enter c: " c
 
-echo "$a + $b * $c" | bc -l
-echo "$a % $b + $c " | bc -l
-echo "$c + $a / $b " | bc -l
-echo "$a * $b + $c" | bc -l
+result1=`echo "$a + $b * $c" | bc -l`
+result2=`echo "$a % $b + $c " | bc -l`
+result3=`echo "$c + $a / $b " | bc -l`
+result4=`echo "$a * $b + $c" | bc -l`
 
 
