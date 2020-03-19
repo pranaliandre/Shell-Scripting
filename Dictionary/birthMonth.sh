ad3742526d35e548b26152e88d0cf7627262374f
pranaliandre <pranaliandre06@gmail.com>
2020-03-19 07:55:48 -0500

    Refactor: to generate a birthmonth of 50 individual between 1992 and 1993 and find all individual having birthmonth in the same month

--- Dictionary/birthMonth.sh
+++ Dictionary/birthMonth.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -x
-
 declare -A birthMonth
-
-for (( i=0;i<50;i++ ))
-do	
-	month=$((RANDOM%12+1))
-	birthMonthDict["$month"]=$((${birthMonthDict["$month"]}+1))
-done
-
-echo "Month : Persons"
-for i in ${!birthMonthDict[@]}
-do
-	echo "$i : ${birthMonthDict[$i]}"
-done | sort -k1 -n
+number=(($RANDOM%2))
+year=$(($number+1992))
+if [[ $year==1992 || $year == 1993 ]]
+then
+	for (( i=0;i<50;i++ ))
+	do
+		month=$((RANDOM%12+1))
+		birthMonthDict["$month"]=$((${birthMonthDict["$month"]}+1))
+	done
+	echo "year:${!year[@]}"
+	echo "Key:${!birthMonthDict[@]}"
+	echo "values:${birthMonthDict[@]}"
+else
+	echo "Not valid year"
+fi
